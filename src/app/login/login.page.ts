e82f04f328035edf41597d26de0dc83ed7552abc
Pablo Aguilar <tatoaguilar95@gmail.com>
2020-05-20 18:01:01 -0400

    Modificacion de texto en interfaz y funcionamiento de servicios
    - Se modifica el texto de la interfaz de Home, Login, Registro de usuario.
    - Se cambia el nombre de los formControlName según nombre de atributos en BD.
    - Se dan funcionalidad a los métodos de iniciar sesión y registro de usuarios
    - Almacenamiento de token de sesion en storage

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { StorageService } from '../services/storage.service';;
+import { AuthConstants } from '../config/auth-constants';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +14,9 @@ export class LoginPage implements OnInit {
   formLogin: FormGroup;
 
   constructor(
-    public formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private storageService: StorageService
   ) { }
 
   ngOnInit() {
@@ -23,8 +28,24 @@ export class LoginPage implements OnInit {
 
   }
 
-  login() {
-    console.log("apreto el boton para ingresar");
+  iniciar_sesion() {
+
+    let credenciales = {
+      "username": this.formLogin.get('email').value,
+      "password": this.formLogin.get('password').value
+    }
+
+    this.authService.login(credenciales).subscribe(
+      (data: any) => {
+        // console.log("token de acceso: " + data.token);
+        this.storageService.store(AuthConstants.AUTH, data.token);
+        // console.log(this.storageService.get(AuthConstants.AUTH));
+      },
+      (error: any) => {
+        console.log(error.message);
+      }
+    );
+
   }
 
 }
