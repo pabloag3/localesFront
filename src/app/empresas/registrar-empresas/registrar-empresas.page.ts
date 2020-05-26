9f7082ff77766e992532728ea459e54e41a193a3
Manuel <manuel.mettel@gmail.com>
2020-05-26 15:10:03 -0400

    Creacion de pagina de registro de empresa

---
+++ src/app/empresas/registrar-empresas/registrar-empresas.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registrar-empresas',
+  templateUrl: './registrar-empresas.page.html',
+  styleUrls: ['./registrar-empresas.page.scss'],
+})
+export class RegistrarEmpresasPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
